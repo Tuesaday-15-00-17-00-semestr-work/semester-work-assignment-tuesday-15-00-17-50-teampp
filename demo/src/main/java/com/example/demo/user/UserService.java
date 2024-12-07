e6202117f1fb3730b9658b044782d02ba5a5b565
Patrik Pocarovsky <xpocarovskyp@stuba.sk>
2024-12-07 19:16:15 +0100

    first commit: basic spring boot + databases, no javaFX

---
+++ demo/src/main/java/com/example/demo/user/UserService.java
@@ -0,0 +1,48 @@
+package com.example.demo.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public void addNewUser(User user) {
+        Optional<User> userOptional =
+                UserRepository.findByUsername(user.getUsername());
+        if(userOptional.isPresent()) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+        userRepository.save(user);
+    }
+
+    /*
+    public List<User> getUsers() {
+        return List.of(
+                new User(
+                        1L,
+                        "patopoco",
+                        "123",
+                        1,
+                        "jbmnt"
+                )
+        );
+
+    }
+
+   */
+}
